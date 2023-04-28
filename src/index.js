8d17b2f7088b7b238428a9333eb327e634253036
Jacob <j31c0@hotmail.com>
2023-04-28 13:04:14 -0500

    Add functions to manage projects

--- src/index.js
+++ src/index.js
@@ -1,20 +1,2 @@
-const task = (title, description, dueDate, priority) => {
-  const sayTask = () => {
-    console.log({ title, description, dueDate, priority });
-  };
-  return { title, description, dueDate, priority, sayTask };
-};
+import { createTodo, addTask, createProject } from "./logic";
 
-const project = [];
-
-const addTask = (project, ...task) => {
-  return project.push(...task);
-};
-
-const firstTask = task("run", "go to the park", "1/1/11", "high");
-const secondTask = task("jump", "go to the city", "1/2/11", "low");
-const thirdTask = task("walk", "go to the mall", "4/1/31", "medium");
-
-addTask(project, firstTask, secondTask, thirdTask);
-
-console.log(project);
---
+++ src/logic.js
@@ -0,0 +1,34 @@
+const createTodo = (title, description, dueDate, priority) => {
+  const sayTask = () => {
+    console.log({ title, description, dueDate, priority });
+  };
+  return { title, description, dueDate, priority, sayTask };
+};
+
+const addTask = (project, ...task) => {
+  return project.push(...task);
+};
+
+const createProject = () => {
+  return [];
+};
+
+const project = [];
+
+const firstTask = createTodo("run", "go to the park", "1/1/11", "high");
+const secondTask = createTodo("jump", "go to the city", "1/2/11", "low");
+const thirdTask = createTodo("walk", "go to the mall", "4/1/31", "medium");
+addTask(project, firstTask, secondTask, thirdTask);
+
+console.log(project);
+project[0].sayTask();
+
+const project1 = createProject("firstProject");
+const firstTask1 = createTodo("dance", "go to the park", "1/1/11", "high");
+const secondTask1 = createTodo("sew", "go to the city", "1/2/11", "low");
+const thirdTask1 = createTodo("sleep", "go to the mall", "4/1/31", "medium");
+addTask(project1, firstTask1, secondTask1, thirdTask1);
+
+console.log(project1);
+
+export { createTodo, addTask, createProject };
